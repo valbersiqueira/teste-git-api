035b68986f038d1e18d424199a95dace471576ce
valber <valberdevelop@gmail.com>
2022-09-23 09:24:28 -0300

    Add paging

---
+++ app/src/main/kotlin/com/br/valber/testegitapi/core/extensions/ActivityExtensions.kt
@@ -0,0 +1,12 @@
+package com.br.valber.testegitapi.core.extensions
+
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T
+) =
+    lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }

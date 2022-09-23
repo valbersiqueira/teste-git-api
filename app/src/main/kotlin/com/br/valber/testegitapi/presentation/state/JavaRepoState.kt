f15f34a8450298a9133b6a4c05b9e5ba15b650b0
valber <valberdevelop@gmail.com>
2022-09-23 20:49:00 -0300

    Removento state

--- app/src/main/kotlin/com/br/valber/testegitapi/presentation/state/JavaRepoState.kt
+++ app/src/main/kotlin/com/br/valber/testegitapi/presentation/state/JavaRepoState.kt
@@ -1,10 +1,5 @@
 package com.br.valber.testegitapi.presentation.state
 
-import com.br.valber.testegitapi.domain.entity.JavaRepo
-
 sealed class JavaRepoState {
-    data class ShowJavaRepo(val javaRepo: JavaRepo) : JavaRepoState()
-    data class ShowError(val message: String) : JavaRepoState()
-
     object Scroll : JavaRepoState()
 }
